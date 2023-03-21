a264e3db3cbd54c5d037d0f24c46342f5b17e05e
Angelo <angelogaliazzi@gmail.com>
2023-03-20 21:17:01 -0300

    vistas basadas en clase adicionadas

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='index'),
+    path('', Inicio.as_view(), name='index'),
     path('crear_autor/', crearautor, name='crear_autor'),
-    path('listar_autor/', listarautor, name='listar_autor'),
+    path('listar_autor/', ListadoAutor.as_view(), name='listar_autor'),
     path('editar_autor/<int:id>', editarautor, name='editar_autor'),
     path('eliminar_autor/<int:id>', eliminarautor, name='eliminar_autor')
 ]
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -2,32 +2,41 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from .forms import AutorForm
 from .models import Autor
+from django.views.generic import TemplateView, ListView
 
 
 # Create your views here.
-def home(request):
-    return render(request, 'index.html')
+
+class Inicio(TemplateView):
+    template_name = 'index.html'
 
 
 def crearautor(request):
     if request.method == 'POST':
         print(request.POST)
-        nom=request.POST.get('nombre')
-        ape=request.POST.get('apellidos')
-        nacio=request.POST.get('nacionalidad')
-        desc=request.POST.get('descripcion')
-        autor=Autor(nombre=nom, apellidos=ape, nacionalidad=nacio, descripcion=desc)
+        nom = request.POST.get('nombre')
+        ape = request.POST.get('apellidos')
+        nacio = request.POST.get('nacionalidad')
+        desc = request.POST.get('descripcion')
+        autor = Autor(nombre=nom, apellidos=ape, nacionalidad=nacio, descripcion=desc)
         autor.save()
         return redirect('libro:index')
     return render(request, 'libro/crear_autor.html')
 
 
 def listarautor(request):
-    autores = Autor.objects.filter(estado=True)
+    autores = Autor.objects.filter(estado=True).order_by('id')
     return render(request, 'libro/listar_autor.html', {'autores': autores})
 
 
-def editarautor(request,id):
+class ListadoAutor(ListView):
+    model = Autor
+    template_name = 'libro/listar_autor.html'
+    context_object_name = 'autores'
+    queryset = Autor.objects.filter(estado=True).order_by('id')
+
+
+def editarautor(request, id):
     autor_form = None
     error = None
     try:
@@ -41,10 +50,11 @@ def editarautor(request,id):
             return redirect('index')
     except ObjectDoesNotExist as e:
         error = e
-    return render(request, 'libro/crear_autor.html', {'autor_form':autor_form, 'error':error})
+    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form, 'error': error})
 
-def eliminarautor(request,id):
+
+def eliminarautor(request, id):
     autor = Autor.objects.get(id=id)
-    autor.estado=False
+    autor.estado = False
     autor.save()
     return redirect('libro:listar_autor')
